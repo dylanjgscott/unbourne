f4f5898b694facef720be719e4d9ddf8c6e379a2
Dylan Scott <dylanjgscott@gmail.com>
2014-03-28 20:22:46 +1100

    Changed cd.c to use realpath.

--- cd.c
+++ cd.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <stdbool.h>
 
+#include "unbourne.h"
+
 #include "cd.h"
 
 /* cd - change directory
@@ -11,28 +13,30 @@
  */
 void cd(char **args)
 {
-	/* check for absolute path */
-	if(args[1][0] == '/')
-	{
-		/* check if chdir was successful */
-		if(chdir(args[1]) == 0)
-		{
-			/* try to set environment */
-			if(setenv(PWD_VAR, args[1], true) != 0)
-			{
-				/* show error */
-				fprintf(stderr, "Error: could not set working directory in environment.\n");
-			}
-		}
-		else
-		{
-			/* show error */
-			fprintf(stderr, "Error: could not change directory to '%s'.\n", args[1]);
-		}
-	}
-	else
-	{
-		/* show error */
-		fprintf(stderr, "Error: You must use an absolute path.\n");
-	}
+
+    /* check if chdir was successful */
+    if(chdir(args[1]) == 0)
+    {
+        /* get the real directory */
+        char *real = realpath(args[1], NULL);
+            /* check if we got a real path */
+        if(real != NULL)
+        {
+            /* try to set environment */
+            if(setenv(PWD_VAR, real, true) != 0)
+            {
+                /* show error */
+                fprintf(stderr, "Error: could not set working directory in environment.\n");
+            }
+        }
+        else
+        {
+            fprintf(stderr, "Error: real path was null.\n");
+        }
+    }
+    else
+    {
+        /* show error */
+        fprintf(stderr, "Error: could not change directory to '%s'.\n", args[1]);
+    }
 }
